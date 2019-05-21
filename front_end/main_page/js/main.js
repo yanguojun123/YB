587f825f4d4e1e0bdcff04889e889e729740465d
hands133 <1960303426@qq.com>
2019-05-21 18:39:40 +0800

    增加登陆窗口，请能够访问服务器的同学把从数据库中取数据的功能写完

--- front_end/main_page/js/main.js
+++ front_end/main_page/js/main.js
@@ -3,72 +3,111 @@
 posx = 0;
 posy = 0;
 
-window.onload = function()
-{
+window.onload = function () {
     hhh();
+    showLoginWindow();
 }
 
-function hhh(){
+function hhh() {
     var logWindowPos = $('#loginandreg_pos');
     posx = logWindowPos.offset().left;
     posy = logWindowPos.offset().top;
     var logWindow = $('#login_window');
-    logWindow.css('left',posx - 150);
+    logWindow.css('left', posx - 150);
     logWindow.css('top', posy + 25);
-    logWindow.css('z-index',100);
+    logWindow.css('z-index', 100);
+}
 
-    logWindow.click(function(e){
-        if(!$(e.target).closest('#login_window').length){
-            logWindow.css('display','none');
-        }
-    })
-    // logWindowPos.bind('click',function(e){
-    //     var e = e || window.event;
-    //     var elem = e.target || e.srcElement;
-    //     while(elem)
-    //     {
-    //         if(elem.id && elem.id == 'login_window')
-    //             return;
-    //         elem = elem.parentNode;
-    //     }
-    //     if(logWindow.css('display') == 'none')
-    //         logWindow.css('display', "block");
-    //     else
-    //         logWindow.css('display', "none");
-    // });
+//显示输入框
+function showLoginWindow() {
+    var logWindowPos = $('#loginandreg_pos');
+    var logWindow = $('#login_window');
+    logWindowPos.click(function (e) {
+        if (logWindow.css('display') == 'none')
+            logWindow.css('display', "block");
+        else
+            logWindow.css('display', "none");
+    });
 }
 
 //账号输入框
-function inputInfo1(id)
-{
-    var target= $('#'+id);
-    target.css('onblur',function(){
+function inputInfo1(id) {
+    var target = $('#' + id);
+    target.css('onblur', function () {
         var value = target[0].value;
-        if(value == '')
+        if (value == '')
             target[0].value = 'account';
     });
 }
 
 //密码输入框
-function inputInfo2(id)
-{
-    var target= $('#'+id);
-    target.css('onblur',function(){
+function inputInfo2(id) {
+    var target = $('#' + id);
+    target.css('onblur', function () {
         var value = target[0].value;
-        if(value == '')
+        if (value == '')
             target[0].value = 'password';
     });
 }
 
-function login()
-{   //获取内容
+//将账号密码发送到服务器进行验证并判断返回消息
+//如果服务器判断成功则计入 session 否则刷新
+function login() {
     var account = $('#logInput1')[0].value;
     var password = $('#logInput2')[0].value;
-    console.log(account);
-    console.log(password);
+    //这里将账号密码发送到服务器
+
+    var xhr = new XMLHttpRequest;
+    xhr.open('post', 'C:/xampp/htdocs/get_secondhand_book_data.php');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
+            //响应完成并且响应码为200或304
+            JSON_data = JSON.parse(xhr.responseText);
+            console.log(JSON_data);
+            var length = getJsonlength(JSON_data);
+            alert("出问题了")
+            var father = document.getElementById('content');
+            document.getElementById("content").innerHTML = "";
+
+            for (var i = 0; i < items; i++) {
+                var newdiv = document.createElement('div');
+                newdiv.innerHTML = JSON_data[i].bookName;
+                newdiv.className = 'message';
+                father.appendChild(newdiv);
+            }
+            for (var i = 0; i < (length / items + 1); i++) {
+                var newbutton = document.createElement('button');
+                newbutton.className = "page_button";
+                newbutton.innerHTML = "第" + (i + 1) + "页";
+                newbutton.value = i;
+                father.appendChild(newbutton);
+            }
+        }
+    };
+
+
+
+    //登录成功返回 true
+    var condition = true;
+    if (condition) {
+        $.session.set(account, password);
+        var input = getForm(account, password);
+    } else {
+        $.session.remove(account);
+    }
 }
 
-function register()
-{
+//注册函数
+function register() {
 
+}
+
+function getForm(account, password) { //这里可以引入 md5 加密
+    var tempInput = document.createElement('form');
+    tempInput.type = 'hidden';
+    tempInput.name = account;
+    tempInput.value = password;
+    return tempInput;
 }
