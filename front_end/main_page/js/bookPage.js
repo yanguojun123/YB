3c82dc3b9ea607b6d779efc277dc3a9a59949506
hands133 <1960303426@qq.com>
2019-05-27 21:59:38 +0800

    添加书的主页面，未完成

---
+++ front_end/main_page/js/bookPage.js
@@ -0,0 +1,22 @@
+//这里是书的主页的 js 文件
+
+$(function() {
+    var magnifierConfig = {
+        magnifier : "#magnifier1",//最外层的大容器
+        width : 300,//承载容器宽
+        height : 300,//承载容器高
+        moveWidth : null,//如果设置了移动盒子的宽度，则不计算缩放比例
+        zoom : 2//缩放比例
+    };
+
+    var _magnifier = magnifier(magnifierConfig);
+
+    /*magnifier的内置函数调用*/
+    /*
+        //设置magnifier函数的index属性
+        _magnifier.setIndex(1);
+
+        //重新载入主图,根据magnifier函数的index属性
+        _magnifier.eqImg();
+    */
+});
